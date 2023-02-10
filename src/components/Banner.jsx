62b77585c64b5a6f3153ec2a41a1c98948513553
Clément Storne <clementstorne@gmail.com>
2023-02-10 18:14:05 +0100

    root page responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,11 @@ import "../index.scss";
 function Banner(props) {
   return (
     <section className="banner">
-      <img src={props.image} alt="Bannière" className="background-image" />
+      <img
+        src={props.image}
+        alt="Bannière"
+        className="banner-background-image"
+      />
       <h1 className="hero">{props.text}</h1>
     </section>
   );
